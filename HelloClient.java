0e6d7b0d01180ee47171bb5db455aac067d7c73a
rony <rony@commlinkinfotech.com>
2018-02-05 16:20:39 +0600

    Implementation of iterator.

--- HelloClient.java
+++ HelloClient.java
@@ -1,6 +1,7 @@
 import HelloApp.Hello;
+import HelloApp.Hello2ListHolder;
 import HelloApp.HelloHelper;
-import HelloApp2.Hello2Holder;
+import HelloApp.SampleIteratorHolder;
 import org.omg.CORBA.ORB;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CosNaming.NamingContextExtHelper;
@@ -10,8 +11,8 @@ public class HelloClient {
 
     public static void main(String args[]) {
         try {
-            // create and initialize the ORB
-            ORB orb = ORB.init(args, null);
+            String defArgs[] = new String[]{"-ORBInitialPort", "1050", "-ORBInitialHost", "10.221.222.191"};
+            ORB orb = ORB.init(defArgs, null);
 
             // get the root naming context
             org.omg.CORBA.Object objRef =
@@ -25,11 +26,31 @@ public class HelloClient {
             helloImpl = HelloHelper.narrow(ncRef.resolve_str(name));
 
             System.out.println("Obtained a handle on server object: " + helloImpl);
+            System.out.println("Testing general hello call...");
             System.out.println(helloImpl.sayHello());
-            Hello2Holder hello2Holder = new Hello2Holder();
-            helloImpl.getHello2(hello2Holder);
-            System.out.println(hello2Holder.value.sayHello2());
-           // helloImpl.shutdown();
+            Hello2ListHolder hello2ListHolder = new Hello2ListHolder();
+            SampleIteratorHolder sampleIteratorHolder = new SampleIteratorHolder();
+            System.out.println("Before server call...");
+            System.out.println(hello2ListHolder.value);
+            System.out.println(sampleIteratorHolder.value);
+            System.out.println("Calling server...");
+            helloImpl.getHello2(10, hello2ListHolder, sampleIteratorHolder);
+            System.out.println("Initial Fetched: " + hello2ListHolder.value[0].id + " to " + hello2ListHolder.value[hello2ListHolder.value.length - 1].id);
+            System.out.println("After server call...");
+            System.out.println(hello2ListHolder.value.length);
+            for (int i = 0; i < 9; i++) {
+                Hello2ListHolder meList = new Hello2ListHolder();
+                sampleIteratorHolder.value.next_n(10, meList);
+                System.out.println("Fetched size " + meList.value.length);
+                if (meList.value.length > 0) {
+                    System.out.println("Fetched: " + meList.value[0].id + " to " + meList.value[meList.value.length - 1].id);
+                }
+            }
+
+            long size = sampleIteratorHolder.value.getLength();
+            System.out.println(size);
+
+            // helloImpl.shutdown();
 
         } catch (Exception e) {
             System.out.println("ERROR : " + e);
--- HelloServer.java
+++ HelloServer.java
@@ -1,26 +1,43 @@
-import HelloApp.Hello;
-import HelloApp.HelloHelper;
-import HelloApp.HelloPOA;
+import HelloApp.*;
 import HelloApp2.Hello2;
-import HelloApp2.Hello2Holder;
-import HelloApp2.Hello2POA;
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.Object;
 import org.omg.CosNaming.NameComponent;
 import org.omg.CosNaming.NamingContextExt;
 import org.omg.CosNaming.NamingContextExtHelper;
 import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.POAPackage.ServantNotActive;
+import org.omg.PortableServer.POAPackage.WrongPolicy;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 class HelloImpl extends HelloPOA {
-    private ORB orb;
-    private Hello2 hello2;
+    static List<Hello2> Hello2Repository = new ArrayList<>(100);
 
-    public void setHello2(Hello2 hello2) {
-        this.hello2 = hello2;
+    static {
+        for (int i = 0; i < 100; i++) {
+            Hello2 hello21 = new Hello2(String.valueOf(i));
+            Hello2Repository.add(hello21);
+        }
+    }
+
+    private ORB orb;
+    private POA rootPoa;
+    private SampleIteratorImpl sampleIterator;
+
+    public void setSampleIterator(SampleIteratorImpl sampleIterator) {
+        this.sampleIterator = sampleIterator;
     }
 
     public void setORB(ORB orb_val) {
         orb = orb_val;
+    }
+
+    public void setRootPoa(POA rootPoa) {
+        this.rootPoa = rootPoa;
     }
 
     // implement sayHello() method
@@ -29,9 +46,24 @@ class HelloImpl extends HelloPOA {
     }
 
     @Override
-    public void getHello2(Hello2Holder hello2Holder) {
-        hello2Holder.value = hello2;
+    public void getHello2(int how_many, Hello2ListHolder hello2List, SampleIteratorHolder iterator) {
+        List<Hello2> listToReturn = new ArrayList<>();
+        for (int i = 0; i <= how_many; i++) {
+            listToReturn.add(Hello2Repository.get(i));
+        }
+        hello2List.value = listToReturn.toArray(new Hello2[how_many]);
+        try {
+            // Add iterator.
+            SampleIteratorImpl sampleIterator = new SampleIteratorImpl(how_many, listToReturn.size() - 1);
+            Object itObject = rootPoa.servant_to_reference(sampleIterator);
+            iterator.value = SampleIteratorHelper.narrow(itObject);
+        } catch (ServantNotActive servantNotActive) {
+            servantNotActive.printStackTrace();
+        } catch (WrongPolicy wrongPolicy) {
+            wrongPolicy.printStackTrace();
+        }
     }
+
 
     // implement shutdown() method
     public void shutdown() {
@@ -39,12 +71,64 @@ class HelloImpl extends HelloPOA {
     }
 }
 
-class Hello2Impl extends Hello2POA {
+class SampleIteratorImpl extends SampleIteratorPOA {
+    private int initialDataReturned = 0;
+    private int lastIdFetched = -1;
+
+    public SampleIteratorImpl(int initialDataReturned, int lastIdFetched) {
+        this.initialDataReturned = initialDataReturned;
+        this.lastIdFetched = lastIdFetched;
+    }
 
     @Override
-    public String sayHello2() {
-        System.out.println("In hello2 implementation.");
-        return "Saying hello 2";
+    public boolean next_n(int how_many, Hello2ListHolder meList) {
+        int remainingObject = getLength() - lastIdFetched;
+        if (how_many == 0 && remainingObject > 0) {
+            return true;
+        }
+
+        boolean dataRemain = true;
+        if (remainingObject < how_many) {
+            dataRemain = false;
+        }
+
+        if (dataRemain) {
+            meList.value = getData(lastIdFetched, how_many);
+            this.lastIdFetched = lastIdFetched + how_many;
+            return true;
+        } else {
+            meList.value = getData(lastIdFetched, remainingObject);
+            destroy();
+            return false;
+        }
+    }
+
+    private Hello2[] getData(int lastIdFetched, int how_many) {
+        Hello2[] hello2s = new Hello2[how_many ];
+        int count = 0;
+        for (int i = (lastIdFetched + 1); i < lastIdFetched + 1 + how_many; i++) {
+            if (i < HelloImpl.Hello2Repository.size()) {
+                hello2s[count] = HelloImpl.Hello2Repository.get(i);
+                count++;
+            }
+        }
+
+        return hello2s;
+    }
+
+    @Override
+    public int getLength() {
+        int size = HelloImpl.Hello2Repository.size() - initialDataReturned;
+        return size;
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            this.finalize();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 }
 
@@ -53,27 +137,24 @@ public class HelloServer {
 
     public static void main(String args[]) {
         try {
-            // create and initialize the ORB
-            ORB orb = ORB.init(args, null);
+            String defArgs[] = new String[]{"-ORBInitialPort", "1050", "-ORBInitialHost", "10.221.222.191"};
+            ORB orb = ORB.init(defArgs, null);
 
             // get reference to rootpoa & activate the POAManager
             POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
             rootpoa.the_POAManager().activate();
 
 
-
-            Hello2Impl hello2ImpL = new Hello2Impl();
-
             // create servant and register it with the ORB
             HelloImpl helloImpl = new HelloImpl();
             helloImpl.setORB(orb);
+            helloImpl.setRootPoa(rootpoa);
 
 
             // get object reference from the servant
             org.omg.CORBA.Object ref = rootpoa.servant_to_reference(helloImpl);
             Hello href = HelloHelper.narrow(ref);
 
-          rootpoa.servant_to_reference(hello2ImpL);
 
             // get the root naming context
             // NameService invokes the name service
@@ -88,8 +169,6 @@ public class HelloServer {
             NameComponent path[] = ncRef.to_name(name);
             ncRef.rebind(path, href);
 
-
-            helloImpl.setHello2(hello2ImpL._this());
             System.out.println("HelloServer ready and waiting ...");
 
             // wait for invocations from clients
